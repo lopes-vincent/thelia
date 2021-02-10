957eb884be5f1b72a9cfe835d727f40f4786dece
Gilles Bourgeat <gilles.bourgeat@gmail.com>
2021-02-10 22:01:35 +0100

    apply cs fixer

--- local/modules/Colissimo/I18n/backOffice/default/de_DE.php
+++ local/modules/Colissimo/I18n/backOffice/default/de_DE.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return [
     '*If you choose this option, the exported orders would not be available on this page anymore' => '*Wenn Sie diese Option auswählen, sind die exportierten Bestellungen auf dieser Seite nicht mehr verfügbar',
     'Actions' => 'Aktionen',
